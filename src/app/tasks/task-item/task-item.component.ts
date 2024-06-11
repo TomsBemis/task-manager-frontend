f1db5ae3cf664b26c1218eb4ab2a8c9b40e8ae60
TomsBemis <toms.bemis@vasscompany.com>
2024-06-11 12:08:43 +0300

    TM-6 Task list component that displays list of tasks from imported JSON file using Task Service

--- src/app/tasks/task-item/task-item.component.ts
+++ src/app/tasks/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-item',
@@ -12,5 +13,9 @@ export class TaskItemComponent {
 
   @Input('taskElement') task!: Task;
   
-  constructor() {}
+  constructor(private taskService: TaskService) {}
+
+  onDeleted(taskTitle: string) {
+    this.taskService.taskDeleted.emit(taskTitle);
+  }
 }
