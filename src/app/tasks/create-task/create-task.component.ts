01a4007bc478e54ece669ab5c1c7a0d2b98d96ac
TomsBemis <toms.bemis@vasscompany.com>
2024-06-11 12:12:06 +0300

    TM-5 TaskCreated event refactored to use Task Service

--- src/app/tasks/create-task/create-task.component.ts
+++ src/app/tasks/create-task/create-task.component.ts
@@ -2,6 +2,7 @@ import { Component, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Task } from '../task.model';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-create-task',
@@ -11,10 +12,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './create-task.component.scss'
 })
 export class CreateTaskComponent {
-  @Output() taskCreated = new EventEmitter<any>();
+
+  constructor(private taskService: TaskService) {}
 
   onCreateTask(taskTitleInput: HTMLInputElement, taskDescriptionInput: HTMLInputElement, taskStatusInput: HTMLInputElement) {
-    this.taskCreated.emit(
+    this.taskService.taskCreated.emit(
       new Task(taskTitleInput.value, taskDescriptionInput.value, new Date(), taskStatusInput.value));
   }
 }
