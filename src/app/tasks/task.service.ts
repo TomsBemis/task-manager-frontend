23cf52df870c27209490540a955b41cbd7af2b0b
TomsBemis <toms.bemis@vasscompany.com>
2024-07-23 12:36:05 +0300

    TM-22 Task service cleanup
     - Removed unnecessary subjects and replaced them with local properties;
     - Removed unnecessary ID generator service because backend already generates ID for objects.

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,5 +1,4 @@
 import { Task, Option, BasicTask } from "./task.model";
-import { IdGeneratorService } from "./id-generator-service";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { beApiRoutes } from "../routes/be-api.routes";
@@ -8,27 +7,27 @@ import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable()
 export class TaskService {
     
-    private taskTypesSubject = new BehaviorSubject<Option[]>([]);
-    private taskStatusesSubject = new BehaviorSubject<Option[]>([]);
-    private tasksSubject = new BehaviorSubject<Task[]>([]);
+    private taskTypes : Option[] = [];
+    private taskStatuses : Option[] = [];
+    private tasks : Task[] = []
     public basicTasksSubject = new BehaviorSubject<BasicTask[]>([]);
 
-    constructor(private idGeneratorService: IdGeneratorService, private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient) {
         this.fetchDataFromDB();
     }
 
     private fetchDataFromDB() {
         // Get task types, task statuses and tasks from DB
         this.httpClient.get<Option[]>(beApiRoutes.taskStatuses).subscribe(responseTaskStatuses => {
-            this.taskStatusesSubject.next(responseTaskStatuses);  
+            this.taskStatuses = responseTaskStatuses;  
         });
 
         this.httpClient.get<Option[]>(beApiRoutes.taskTypes).subscribe(responseTaskTypes => {
-            this.taskTypesSubject.next(responseTaskTypes);           
+            this.taskTypes = responseTaskTypes;           
         });
 
         this.httpClient.get<Task[]>(beApiRoutes.tasks).subscribe(responseTasks => {
-            this.tasksSubject.next(responseTasks);    
+            this.tasks = responseTasks;    
         });
 
         this.basicTasksSubject.next(this.getTasks());
@@ -43,19 +42,19 @@ export class TaskService {
     }
 
     private convertBasicTaskToTask(basicTask: BasicTask) : Task | null {
-        let taskIndex: number = this.tasksSubject.getValue().findIndex(task => task.id == basicTask.id);
+        let taskIndex: number = this.tasks.findIndex(task => task.id == basicTask.id);
         if(taskIndex === -1) return null;
-        return this.tasksSubject.getValue()[taskIndex];
+        return this.tasks[taskIndex];
     }
 
     private convertTasksToBasicTasks(tasks: Task[]) : BasicTask[] {
         let basicTasks : BasicTask[] = [];
-        this.tasksSubject.getValue().forEach(task => basicTasks.push(this.convertTaskToBasicTask(task)));
+        this.tasks.forEach(task => basicTasks.push(this.convertTaskToBasicTask(task)));
         return basicTasks;
     }
 
     public getTasks() : BasicTask[] {
-        return this.convertTasksToBasicTasks(this.tasksSubject.getValue()).slice()
+        return this.convertTasksToBasicTasks(this.tasks).slice()
         .sort(
             (taskA, taskB) => {
                 return (taskA.title < taskB.title) ? -1 : (taskA.title > taskB.title) ? 1 : 0
@@ -67,12 +66,12 @@ export class TaskService {
     }
 
     public addTask(task: Task) {
-        let oldTaskList = this.tasksSubject.getValue().slice();
+        let oldTaskList = this.tasks.slice();
         this.httpClient.post<Task | null>(beApiRoutes.tasks, task)
         .subscribe(createdTask => {
             if(createdTask) {
                 oldTaskList.push(createdTask);
-                this.tasksSubject.next(oldTaskList);
+                this.tasks = oldTaskList;
                 this.basicTasksSubject.next(this.getTasks());
             }
         });
@@ -80,27 +79,25 @@ export class TaskService {
 
     public updateTask(id: number, editedTask: Task){
         // Update the task if the ids match
-        this.tasksSubject.next(this.tasksSubject.getValue().map((task: Task) => {
+        this.tasks = this.tasks.map((task: Task) => {
             if(task.id === id) task = editedTask;
             return task
-        }));
+        });
     }
 
     public deleteTask(taskId: number) {
         // Task title is it's unique identifier (needs validation on creation)
-        this.tasksSubject.next(
-            this.tasksSubject.getValue().splice(
-                this.tasksSubject.getValue().findIndex(task => task.id == taskId), 
+        this.tasks = this.tasks.splice(
+                this.tasks.findIndex(task => task.id == taskId), 
                 1
-            )
-        );
+            );
     }
 
     public getTaskTypes() {
-        return this.taskTypesSubject.getValue();
+        return this.taskTypes;
     }
 
     public getTaskStatuses() {
-        return this.taskStatusesSubject.getValue();
+        return this.taskStatuses;
     }
 }
