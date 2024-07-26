f4ed05088e21ab734dcebfb4bf6b18df87b49c40
TomsBemis <toms.bemis@vasscompany.com>
2024-07-26 11:15:31 +0300

    TM-20 Pull Request #11 Requested changes 1

--- src/app/tasks/task-details/task-details.component.ts
+++ src/app/tasks/task-details/task-details.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Task, Option, BasicTask } from '../task.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TaskService } from '../task.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DatePipe, KeyValuePipe } from '@angular/common';
-import { map, switchMap } from 'rxjs';
+import { map, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-task-details',
@@ -17,12 +17,13 @@ import { map, switchMap } from 'rxjs';
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.scss'
 })
-export class TaskDetailsComponent implements OnInit {
+export class TaskDetailsComponent implements OnInit, OnDestroy {
 
   task: Task | null = null;
   editMode: boolean = false;
   taskTypes : Option[] = this.taskService.getTaskTypes();
   taskStatuses : Option[] = this.taskService.getTaskStatuses();
+  fetchTaskDetailsSubscription : Subscription = new Subscription();
 
   editTaskForm: FormGroup = new FormGroup({
     title: new FormControl(),
@@ -40,29 +41,27 @@ export class TaskDetailsComponent implements OnInit {
     this.editMode = !this.editMode;
   }
   
-  onSubmit () {
+  onSubmit (task : Task) {
     // Get filled out form data using form group
 
-    if(this.task) {
-      this.taskService.updateTask(
-        this.task.id,
-        {
-          id: this.task.id,
-          title: this.editTaskForm.get('title')?.value,
-          description: this.editTaskForm.get('description')?.value,
-          type: this.taskTypes.find( taskType => 
-            taskType.value == this.editTaskForm.get('type')?.value
-          ) as Option,
-          status: this.taskStatuses.find(taskStatus => 
-            taskStatus.value == this.editTaskForm.get('status')?.value
-          ) as Option,
-          updatedAt: new Date(),
-          createdAt: this.task.createdAt,
-        }
-      );
-  
-      this.router.navigate(['tasks']);
-    }    
+    this.taskService.updateTask(
+      task.id,
+      {
+        id: task.id,
+        title: this.editTaskForm.get('title')?.value,
+        description: this.editTaskForm.get('description')?.value,
+        type: this.taskTypes.find( taskType => 
+          taskType.value == this.editTaskForm.get('type')?.value
+        ) as Option,
+        status: this.taskStatuses.find(taskStatus => 
+          taskStatus.value == this.editTaskForm.get('status')?.value
+        ) as Option,
+        updatedAt: new Date(),
+        createdAt: task.createdAt,
+      }
+    );
+
+    this.router.navigate(['tasks']);  
   }
 
   constructor(private router: Router, private route: ActivatedRoute, private taskService: TaskService) {}
@@ -71,24 +70,28 @@ export class TaskDetailsComponent implements OnInit {
     
     // Get task id from route parameters then pass it as argument for task service
     // set the component task when async method is done
-    this.route.params.pipe(
+    this.fetchTaskDetailsSubscription = this.route.params.pipe(
       map(params => params['id'] as number),
       switchMap(taskId => {
         return this.taskService.getTask(taskId)
       })).subscribe(responseTask => {
         this.task = responseTask;
+
+        // Fill out edit task form as soon as the task is fetched from BE
+        this.editTaskForm = new FormGroup({
+          title: new FormControl(this.task?.title, [
+            Validators.required,
+            this.validateTitleUnique.bind(this)
+          ]), //Custom validator for unique title
+          description: new FormControl(this.task?.description),
+          type: new FormControl(this.task?.type?.value, Validators.required),
+          status: new FormControl(this.task?.status?.value, Validators.required)
+        });
       });
+  }
 
-    this.editTaskForm = new FormGroup({
-      title: new FormControl(this.task?.title, [
-        Validators.required,
-        this.validateTitleUnique.bind(this)
-      ]), //Custom validator for unique title
-      description: new FormControl(this.task?.description),
-      type: new FormControl(this.task?.type?.value, Validators.required),
-      status: new FormControl(this.task?.status?.value, Validators.required)
-    });
-
+  ngOnDestroy(): void {
+    this.fetchTaskDetailsSubscription.unsubscribe();
   }
 
   validateTitleUnique(control: FormControl): {[s: string]: boolean} | null {
